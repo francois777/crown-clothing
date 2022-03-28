adbb13bb1a13d946b710cb9462cc3fc5cff266b9
Francois van der Hoven <francoisvanderhoven@gmail.com>
2022-03-28 21:33:57 +1000

    fixed last deployment error

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -23,7 +23,7 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
