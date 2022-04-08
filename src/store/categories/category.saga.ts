aa3fb5fdfc75fcc6b3b63f068421754bfcaee0a5
Francois van der Hoven <francoisvanderhoven@gmail.com>
2022-04-08 16:54:18 +1000

    New utility modules; updated tsconfig.json; No more errors

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { takeLatest, all, call, put } from 'redux-saga/effects'
+import { takeLatest, all, call, put } from 'typed-redux-saga'
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils'
 
@@ -7,14 +7,14 @@ import {
   fetchCategoriesFailed
 } from './category.action'
 
-import CATEGORIES_ACTION_TYPES from './category.types'
+import {CATEGORIES_ACTION_TYPES} from './category.types'
 
 export function* fetchCategoriesAsync() {
   try {
-    const categories = yield call(getCategoriesAndDocuments, 'categories')
-    yield put(fetchCategoriesSuccess(categories))
+    const categories = yield* call(getCategoriesAndDocuments)
+    yield* put(fetchCategoriesSuccess(categories))
   } catch (error) {
-    yield put(fetchCategoriesFailed(error))
+    yield* put(fetchCategoriesFailed(error as Error))
   }
 }
 
